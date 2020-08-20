17eee9d9e929438b1b7b69ca4adea07432c910c5
Mizuki <tibbers622@gmail.com>
2020-08-20 15:58:33 +0900

    add untracked files

---
+++ Creating-and-Combining-Views/SwiftUi-tutorials/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  SwiftUIView.swift
+//  SwiftUi-tutorials
+//
+//  Created by sango.m.ab on 2020/08/19.
+//  Copyright © 2020 sango.m.ab. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+        .shadow(radius: 10)
+        
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
