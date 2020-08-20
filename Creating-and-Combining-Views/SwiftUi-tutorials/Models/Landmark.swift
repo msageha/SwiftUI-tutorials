7114dcc1bd5c4bcca7bd1f56bb2fb7c70629fb35
Mizuki <tibbers622@gmail.com>
2020-08-20 15:57:02 +0900

    add

---
+++ Creating-and-Combining-Views/SwiftUi-tutorials/Models/Landmark.swift
@@ -0,0 +1,48 @@
+//
+//  Landmark.swift
+//  SwiftUi-tutorials
+//
+//  Created by sango.m.ab on 2020/08/19.
+//  Copyright © 2020 sango.m.ab. All rights reserved.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    fileprivate var imageName: String
+    fileprivate var coordinates: Coordinates
+    var state: String
+    var park: String
+    var category: Category
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
+    
+    enum Category: String, CaseIterable, Codable, Hashable {
+        case faeatured = "Featured"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+    }
+}
+
+
+extension Landmark {
+    var image: Image {
+        ImageStore.shared.image(name: imageName)
+    }
+}
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
+
+struct Landmark_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
